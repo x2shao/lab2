c246ab05f115e532411408a30727d89d5e880cd6
Xinyu Shao <x2shao@ucsd.edu>
2018-01-18 11:19:50 -0800

    added Javascript

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,17 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+$("a.thumbnail").click(projectClick);
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    $(this).css("background-color", "#7fff00");
+}
 }
